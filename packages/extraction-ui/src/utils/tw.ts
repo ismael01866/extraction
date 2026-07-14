0182b7bcab68e61e8cb655a35708eb190fffd875
Ismael Mariscal <ismael01866@hotmail.com>
2026-07-14 07:36:54 -0600

    package json fixes

--- packages/extraction-ui/src/utils/tw.ts
+++ packages/extraction-ui/src/utils/tw.ts
@@ -1,34 +1,6 @@
+import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-type ClassValue =
-  | string
-  | number
-  | boolean
-  | null
-  | undefined
-  | Record<string, boolean>
-  | ClassValue[];
-
-function cx(...inputs: ClassValue[]): string {
-  const classes: string[] = [];
-
-  for (const input of inputs) {
-    if (!input) continue;
-
-    if (typeof input === 'string' || typeof input === 'number') {
-      classes.push(String(input));
-    } else if (Array.isArray(input)) {
-      classes.push(cx(...input));
-    } else {
-      for (const [key, value] of Object.entries(input)) {
-        if (value) classes.push(key);
-      }
-    }
-  }
-
-  return classes.join(' ');
-}
-
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(cx(...inputs));
+  return twMerge(clsx(inputs));
 }
