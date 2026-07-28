e1ea2b3b0f31ad7ca8f9926402183afe2ed2d327
Ismael Mariscal <ismael01866@hotmail.com>
2026-07-28 15:36:22 -0600

    chore:docs update

--- apps/docs/content/theming/_meta.js
+++ apps/docs/content/theming/_meta.js
@@ -1,6 +1,6 @@
 export default {
-  theming: {
-    title: 'Theming',
+  colors: {
+    title: 'Colors',
   },
   behavior: {
     title: 'Behavior',
---
+++ apps/docs/content/theming/colors/_meta.js
@@ -0,0 +1,24 @@
+export default {
+  palette: {
+    title: 'Palette',
+  },
+  extra: {
+    title: 'Extra Palettes',
+  },
+  customizing: {
+    title: 'Customizing',
+  },
+  creating: {
+    title: 'Adding Colors',
+  },
+
+  tonal: {
+    title: 'Tonal Colors',
+  },
+  variants: {
+    title: 'Variants',
+  },
+  'color-mode': {
+    title: 'Color Mode',
+  },
+};
